6b3f67f38520d341c7289db0d29eca6c3055c31d
Aml2030 <118318264+Aml2030@users.noreply.github.com>
2023-03-07 03:37:50 +0300

    Update main.js

--- main.js
+++ main.js
@@ -4,50 +4,18 @@ let tasksDiv = document.querySelector(".tasks");
 
 let arrayOfTasks = [];
 
-getDataFromLocalStorage();
-if (window.localStorage.getItem("tasks")) {
-  arrayOfTasks = JSON.parse(window.localStorage.getItem("tasks"));
+getDataFromLocalStorage(); //[1]
+
+//Get From Local [1]
+function getDataFromLocalStorage() {
+  let data = window.localStorage.getItem("tasks");
+  if (data) {
+    let tasks = JSON.parse(data);
+    arrayOfTasks = tasks;
+    addElementsToPageFrom(tasks);
+  }
 }
-//Add Task
-submit.onclick = function () {
-  if (input.value !== "") {
-    addTaskToArray(input.value);
-    input.value = "";
-  }
-};
-
-tasksDiv.addEventListener("click", (e) => {
-  if (e.target.classList.contains("del")) {
-    e.target.parentElement.remove();
-    deleteTasksWith(e.target.parentElement.getAttribute("data-id"));
-    //new
-    if (tasksDiv.children[0].className == "delAll") {
-      tasksDiv.children[0].remove();
-    }
-  }
-  if (e.target.classList.contains("task")) {
-    e.target.classList.toggle("done");
-    togleStatusTaskWith(e.target.getAttribute("data-id"));
-  }
-  //new
-  if (e.target.classList.contains("delAll")) {
-    deleteAllTasks();
-  }
-});
-
-function addTaskToArray(taskText) {
-  const task = {
-    id: Date.now(),
-    title: taskText,
-    completed: false,
-  };
-  arrayOfTasks.push(task);
-  addElementsToPageFrom(arrayOfTasks);
-  //Time to Add Data to Local Storage
-  addDataToLocalStorageFrom(arrayOfTasks);
-}
-
-//Create Elements
+//We distributed existing & New Elements [2].
 function addElementsToPageFrom(arrayOfTasks) {
   tasksDiv.innerHTML = "";
   arrayOfTasks.forEach((task) => {
@@ -64,7 +32,7 @@ function addElementsToPageFrom(arrayOfTasks) {
     div.appendChild(span);
     tasksDiv.appendChild(div);
   });
-  //new
+  //added to project recently
   if (tasksDiv.children.length !== 0) {
     let deleteall = document.createElement("span");
     deleteall.className = "delAll";
@@ -73,24 +41,57 @@ function addElementsToPageFrom(arrayOfTasks) {
   }
 }
 
+//Add Task [3]
+submit.onclick = () => {
+  if (input.value !== "") {
+    addTaskToArr(input.value);
+    input.value = "";
+  }
+};
+
+//Create the New Elements [4]
+function addTaskToArr(taskText) {
+  const task = {
+    id: Date.now(),
+    title: taskText,
+    completed: false,
+  };
+  arrayOfTasks.push(task);
+  addElementsToPageFrom(arrayOfTasks);
+  //Time to Add Data to Local Storage
+  addDataToLocalStorageFrom(arrayOfTasks);
+}
+
 //Add to Local
 function addDataToLocalStorageFrom(arrayOfTasks) {
   window.localStorage.setItem("tasks", JSON.stringify(arrayOfTasks));
 }
-//Get From Local
-function getDataFromLocalStorage() {
-  let data = window.localStorage.getItem("tasks");
-  if (data) {
-    let tasks = JSON.parse(data);
-    addElementsToPageFrom(tasks);
+
+//if we click on one of the tasks
+tasksDiv.addEventListener("click", (e) => {
+  if (e.target.classList.contains("del")) {
+    e.target.parentElement.remove();
+    deleteTasksWith(e.target.parentElement.getAttribute("data-id"));
+    //added to project recently
+    if (tasksDiv.children[0].className == "delAll") {
+      tasksDiv.children[0].remove();
+    }
   }
-}
+  if (e.target.classList.contains("task")) {
+    e.target.classList.toggle("done");
+    toggleStatusTaskWith(e.target.getAttribute("data-id"));
+  }
+  //added to project recently
+  if (e.target.classList.contains("delAll")) {
+    deleteAllTasks();
+  }
+});
 
 function deleteTasksWith(taskId) {
   arrayOfTasks = arrayOfTasks.filter((task) => task.id != taskId);
-  addDataToLocalStorageFrom(arrayOfTasks);
+  addDataToLocalStorageFrom(arrayOfTasks); //Update Local
 }
-function togleStatusTaskWith(taskId) {
+function toggleStatusTaskWith(taskId) {
   for (let i = 0; i < arrayOfTasks.length; i++) {
     if (arrayOfTasks[i].id == taskId) {
       arrayOfTasks[i].completed == false
@@ -98,13 +99,19 @@ function togleStatusTaskWith(taskId) {
         : (arrayOfTasks[i].completed = false);
     }
   }
-  addDataToLocalStorageFrom(arrayOfTasks);
+  addDataToLocalStorageFrom(arrayOfTasks); //Update Local
 }
 
-//new
+//added to project recently
 function deleteAllTasks() {
   tasksDiv.innerHTML = "";
   arrayOfTasks = [];
   addDataToLocalStorageFrom(arrayOfTasks);
   window.localStorage.removeItem("tasks");
 }
+
+document.onkeyup = function (e) {
+  if (e.key == "Enter") {
+    submit.click();
+  }
+};
